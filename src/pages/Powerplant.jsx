0612da2210f78fc33de664b67738d1a0d9aba65f
unknown <christianjhegggfer@gmail.com>
2023-04-04 16:49:01 +0800

    routes and navigation fixed

--- src/pages/Powerplant.jsx
+++ src/pages/Powerplant.jsx
@@ -1,9 +1,299 @@
-import React from 'react'
+import React, { useCallback, useMemo, useState } from 'react';
+import MaterialReactTable from 'material-react-table';
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  MenuItem,
+  Stack,
+  TextField,
+  Tooltip,
+} from '@mui/material';
+import Groups2Icon from '@mui/icons-material/Groups2';
+import FactoryIcon from '@mui/icons-material/Factory';
+import { Delete, Edit } from '@mui/icons-material';
+import { data, address } from '../data/mockData';
 
-const Powerplat = () => {
+const Site = () => {
+  const [createModalOpen, setCreateModalOpen] = useState(false);
+  const [tableData, setTableData] = useState(() => data);
+  const [validationErrors, setValidationErrors] = useState({});
+
+  const handleCreateNewRow = (values) => {
+    tableData.push(values);
+    setTableData([...tableData]);
+  };
+
+  const handleSaveRowEdits = async ({ exitEditingMode, row, values }) => {
+    if (!Object.keys(validationErrors).length) {
+      tableData[row.index] = values;
+      //send/receive api updates here, then refetch or update local table data for re-render
+      setTableData([...tableData]);
+      exitEditingMode(); //required to exit editing mode and close modal
+    }
+  };
+
+  const handleCancelRowEdits = () => {
+    setValidationErrors({});
+  };
+
+  const handleDeleteRow = useCallback(
+    (row) => {
+      if (
+        !confirm(`Are you sure you want to delete ${row.getValue('Name')}`)
+      ) {
+        return;
+      }
+      //send api delete request here, then refetch or update local table data for re-render
+      tableData.splice(row.index, 1);
+      setTableData([...tableData]);
+    },
+    [tableData],
+  );
+
+  const getCommonEditTextFieldProps = useCallback(
+    (cell) => {
+      return {
+        error: !!validationErrors[cell.id],
+        helperText: validationErrors[cell.id],
+        onBlur: (event) => {
+          const isValid =
+            cell.column.id === 'email'
+              ? validateEmail(event.target.value)
+              : cell.column.id === 'age'
+              ? validateAge(+event.target.value)
+              : validateRequired(event.target.value);
+          if (!isValid) {
+            //set validation error for cell if invalid
+            setValidationErrors({
+              ...validationErrors,
+              [cell.id]: `${cell.column.columnDef.header} is required`,
+            });
+          } else {
+            //remove validation error for cell if valid
+            delete validationErrors[cell.id];
+            setValidationErrors({
+              ...validationErrors,
+            });
+          }
+        },
+      };
+    },
+    [validationErrors],
+  );
+
+  const formColumns = useMemo(
+    () => [
+      {
+        accessorKey: 'Name',
+        header: 'Name',
+        size: 140,
+        muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
+          ...getCommonEditTextFieldProps(cell),
+        }),
+      },
+      {
+        accessorKey: 'abbreviationName',
+        header: 'Abbreviation Name',
+        size: 140,
+        muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
+          ...getCommonEditTextFieldProps(cell),
+        }),
+      },
+      {
+        accessorKey: 'primaryGroup',
+        header: 'Primary Group',
+        size: 140,
+        muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
+          ...getCommonEditTextFieldProps(cell),
+        }),
+      },
+      {
+        accessorKey: 'primaryLanguage',
+        header: 'Primary Language',
+        size: 140,
+        muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
+          ...getCommonEditTextFieldProps(cell),
+        }),
+      },
+      {
+        accessorKey: 'address',
+        header: 'Address',
+        muiTableBodyCellEditTextFieldProps: {
+          select: true, // add select property for dropdown
+          children: address.map((address) => (
+            <MenuItem key={address} value={address}>
+              {address}
+            </MenuItem>
+          )),
+        },
+      },
+    ],
+    [getCommonEditTextFieldProps, address] // include address in dependencies array
+  );
+
+  const tableColumns = useMemo(
+    () => [
+      {
+        accessorKey: 'Name',
+        header: 'Name',
+        size: 140,
+        muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
+          ...getCommonEditTextFieldProps(cell),
+        }),
+      },
+      {
+        accessorKey: 'address',
+        header: 'Address',
+        muiTableBodyCellEditTextFieldProps: {
+          select: true, // add select property for dropdown
+          children: address.map((address) => (
+            <MenuItem key={address} value={address}>
+              {address}
+            </MenuItem>
+          )),
+        },
+      },
+    ],
+    [getCommonEditTextFieldProps, address] // include address in dependencies array
+  );
+
+  
+
   return (
-    <div>Powerplant</div>
-  )
-}
+    <>
+      <MaterialReactTable
+        displayColumnDefOptions={{
+          'mrt-row-actions': {
+            muiTableHeadCellProps: {
+              align: 'center',
+            },
+            size: 120,
+          },
+        }}
+        columns={tableColumns}
+        data={tableData}
+        editingMode="modal" //default
+        enableColumnOrdering
+        enableEditing
+        onEditingRowSave={handleSaveRowEdits}
+        onEditingRowCancel={handleCancelRowEdits}
+        renderRowActions={({ row, table }) => (
+          <Box sx={{ display: 'flex', justifyContent: 'center', gap: '1rem' }}>
+            <Tooltip arrow placement="right" title="Edit">
+              <IconButton color="warning" onClick={() => table.setEditingRow(row)}>
+                <Edit/>
+              </IconButton>
+            </Tooltip>
+            <Tooltip arrow placement="right" title="Delete">
+              <IconButton color="error" onClick={() => handleDeleteRow(row)}>
+                <Delete />
+              </IconButton>
+            </Tooltip>
+            <Tooltip arrow placement="right" title="Powerplants">
+              <Button color="secondary" onClick={() => {alert("Navigate to Powerplant")}}>
+                <FactoryIcon/>
+              </Button>
+            </Tooltip>
+            <Tooltip arrow placement="right" title="Edit Groups">
+              <Button color="success" onClick={() => {alert("Navigate to groups")}}>
+                <Groups2Icon/>
+              </Button>
+            </Tooltip>
+          </Box>
+        )}
+        renderTopToolbarCustomActions={() => (
+          <Box sx={{ display: 'flex', gap: '1rem', p: '4px' }}>
+            <Button
+              style={{backgroundColor: '#99cc33'}}
+              onClick={() => setCreateModalOpen(true)}
+              variant="contained"
+            >
+              CREATE POWERPLANT
+            </Button>
+            <Button
+              style={{backgroundColor: 'gray'}}
+              color="warning"
+              onClick={() => {console.log("test")}}
+              variant="contained"
+            >
+              FIX PERMISSIONS
+            </Button>
+          </Box>
+        )}
+      />
+      <CreateNewSite
+        columns={formColumns}
+        open={createModalOpen}
+        onClose={() => setCreateModalOpen(false)}
+        onSubmit={handleCreateNewRow}
+      />
+      </>
+  );
+};
 
-export default Powerplat
+//example of creating a mui dialog modal for creating new rows
+export const CreateNewSite = ({ open, columns, onClose, onSubmit }) => {
+  const [values, setValues] = useState(() =>
+    columns.reduce((acc, column) => {
+      acc[column.accessorKey ?? ''] = '';
+      return acc;
+    }, {}),
+  );
+
+  const handleSubmit = () => {
+    //put your validation logic here
+    onSubmit(values);
+    onClose();
+  };
+
+  return (
+    <Dialog open={open}>
+      <DialogTitle textAlign="center">Create New Site</DialogTitle>
+      <DialogContent>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <Stack
+            sx={{
+              width: '100%',
+              minWidth: { xs: '300px', sm: '360px', md: '400px' },
+              gap: '1.5rem',
+            }}
+          >
+            {columns.map((column) => (
+              <TextField
+                key={column.accessorKey}
+                label={column.header}
+                name={column.accessorKey}
+                onChange={(e) =>
+                  setValues({ ...values, [e.target.name]: e.target.value })
+                }
+              />
+            ))}
+          </Stack>
+        </form>
+      </DialogContent>
+      <DialogActions sx={{ p: '1.25rem' }}>
+        <Button onClick={onClose} color="error">Cancel</Button>
+        <Button color="primary" onClick={handleSubmit} variant="contained">
+          Save
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+const validateRequired = (value) => !!value.length;
+const validateEmail = (email) =>
+  !!email.length &&
+  email
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    );
+const validateAge = (age) => age >= 18 && age <= 50;
+
+export default Site;
