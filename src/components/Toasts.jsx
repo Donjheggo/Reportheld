1444a34dca7ec2dc0f3e356d1cd74219d8d08f61
unknown <christianjhegggfer@gmail.com>
2023-05-04 21:15:07 +0800

    added customized toastify component

---
+++ src/components/Toasts.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { toast } from "react-toastify";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
+export const ToastSuccess = (description) => {
+  toast.success(description, {
+    icon: <CheckCircleIcon sx={{ color: "#99cc33" }} />,
+  });
+  return true;
+};
+
+const Toasts = {
+  ToastSuccess,
+};
+
+export default Toasts;
